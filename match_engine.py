b5ae39b440276fe8dd45ea358bd3318fbdbc3e83
SauravKum2603 <s.kum2603@gmail.com>
2025-07-22 21:20:23 +0530

    Add files via upload

---
+++ match_engine.py
@@ -0,0 +1,9 @@
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+def calculate_similarity(resume_text, job_desc):
+    texts = [resume_text, job_desc]
+    tfidf = TfidfVectorizer().fit_transform(texts)
+    score = cosine_similarity(tfidf[0:1], tfidf[1:2])[0][0]
+    return round(score * 100, 2)
